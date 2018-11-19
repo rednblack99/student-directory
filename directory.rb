5fcdb6c2735e91be1dc9e3588347faa525fad5d3
Adam Lodowski <adamlodowski@gmail.com>
2018-11-19 19:42:32 +0000

    Added variable cohorts to Directory.rb

--- directory.rb
+++ directory.rb
@@ -1,28 +1,28 @@
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # creates empty array
+  # creates empty array and rounds counter
   students = []
   # user inputs the first name
   name = gets.chomp
   # while name is not nil, repeat this code
-  
   while !name.empty? do
     # add the student to the array as a hash
-    puts "What is #{name}'s star sign?"
-    sign = gets.chomp
-    puts "What is #{name}'s date of birth?"
-    dob = gets.chomp
-    students << {name: name, cohort: :november, sign: sign, dob: dob}
+    cohort = "No cohort assigned"
+    until cohort == "November" || cohort == "October"
+      puts "What cohort is #{name} in?"
+      cohort = gets.chomp.capitalize
+    end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "What is the next student's name?"
     name = gets.chomp
   end
-  
+   
   # return the array of students
   students
-end
+ end
 
 def print_header
   puts "The students of Villains Academy are:"
@@ -31,7 +31,7 @@ end
 
 def print(students)
   students.each_with_index do |student, number|
-    to_print = "#{number+1}. #{student[:name]} (#{student[:cohort]} cohort). Born #{student[:dob]} so they are a #{student[:sign]}"
+    to_print = "#{number+1}. #{student[:name]} (#{student[:cohort]} cohort)}"
     puts to_print.center(to_print.length+4)
   end
 end
