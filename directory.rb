57d46a783a07757e94cfbcb298f8edc11037c725
Adam Lodowski <adamlodowski@gmail.com>
2018-11-19 17:37:01 +0000

    Added student numbers and print students with first letter A to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,24 @@
 
 #Adding students to array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked With of the West", cohort: :november},
-  {name: "The Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+
+def input_students
+  puts "Please enter the names of the students:"
+  puts "To finish, just hit return twice"
+  #Creates empty array
+  students = []
+  #Gets students name
+  name = gets.chomp
+  #Unless names is empty, loop code
+  while !name.empty? do
+    #Adds student to the array as a hash
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    # Takes the next name
+    name = gets.chomp
+  end
+  #returns complete students array
+  students
+end
 
 def print_header
   puts "The students of Villains Academy:"
@@ -20,8 +26,10 @@ def print_header
 end
 
 def print(names)
-  names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  names.each_with_index do |student, number|
+    if student[:name].start_with?("A")
+      puts "#{number+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -29,6 +37,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
