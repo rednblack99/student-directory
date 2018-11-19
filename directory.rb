211a55cf9388058d09bd56c929d0b7aa1c337463
Adam Lodowski <adamlodowski@gmail.com>
2018-11-19 18:14:15 +0000

    Directory.rb uses while statement rather than .each

--- directory.rb
+++ directory.rb
@@ -5,36 +5,41 @@ def input_students
   puts "Please enter the names of the students:"
   puts "To finish, just hit return twice"
   #Creates empty array
-  students = []
+  $students = []
+  $student_count = 0
   #Gets students name
   name = gets.chomp
   #Unless names is empty, loop code
   while !name.empty? do
     #Adds student to the array as a hash
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
+    $student_count += 1
+    $students << [name, :november]
+    puts "Now we have #{$student_count} students"
     # Takes the next name
     name = gets.chomp
   end
   #returns complete students array
-  students
+  $students
 end
 
 def print_header
-  puts "The students of Villains Academy with names less than 12 characters:"
+  puts "The students of Villains Academy are:"
   puts "-------------"
 end
 
 def print(names)
-  names.each_with_index do |student, number|
-    if student[:name].length < 12
-      puts "#{number+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  rounds = 0
+  student_index = 0
+  while rounds < $student_count do
+    name_to_print = $students[rounds][0]
+    cohort_to_print = $students[rounds][1]
+    puts "#{name_to_print} (#{cohort_to_print} cohort)"
+    rounds += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{$student_count} great students"
 end
 
 students = input_students
