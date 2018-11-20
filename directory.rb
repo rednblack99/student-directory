1a73f5d5f1196f3f1cea186caf3e7bb10c170941
Adam Lodowski <adamlodowski@gmail.com>
2018-11-20 15:43:33 +0000

    directory.rb now loads existing students from file

--- directory.rb
+++ directory.rb
@@ -6,6 +6,7 @@ def print_menu
   puts "1. Input new students"
   puts "2. Show existing students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -27,6 +28,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -74,6 +77,15 @@ def input_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
